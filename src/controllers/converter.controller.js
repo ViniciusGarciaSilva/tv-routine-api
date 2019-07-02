77b6cb95b1651d70e117caf218b968d907528b7d
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2019-07-02 17:32:46 -0300

    Number logic

--- src/controllers/converter.controller.js
+++ src/controllers/converter.controller.js
@@ -1,10 +1,13 @@
 const parser = require('../services/parser.service')
+const interpreter = require('../services/interpreting.service')
 
 exports.post = async function (req, res, next) {
   const input = req.body
-  const output = parser.parse(input)
+  const parsed = parser.parse(input)
+  console.log('Parsed: ', parsed)
+  const interpreted = interpreter.interpretate(parsed)
   res.status(200).send({
-    data: output
+    data: interpreted
   })
   // res.status(400).send({
   //  Erro: `${error}`
--- src/services/interpreting.service.js
+++ src/services/interpreting.service.js
@@ -1,29 +1,71 @@
 function interpretate (data) {
+  console.log('Interpretating')
+  let isFinished = true
+  let lastChannel = 500
+  let channel = 500
+  let nextChannel = 500
+  let isMenu = false
+  console.log(data)
   for (let i = 0; i < data.length; i++) {
-    switch (data) {
+    console.log(data[i])
+    switch (data[i].button) {
       // CHANNEL
       case 'NET_ONE':
-      case 'NET_TWO':
-      case 'NET_THREE':
-      case 'NET_FOUR':
-      case 'NET_FIVE':
-      case 'NET_SIX':
-      case 'NET_SEVEN':
-      case 'NET_EIGHT':
-      case 'NET_NINE':
-      case 'NET_ZERO':
+        nextChannel = parseChannel(nextChannel, 1)
         break
+      case 'NET_TWO':
+        nextChannel = parseChannel(nextChannel, 2)
+        break
+      case 'NET_THREE':
+        nextChannel = parseChannel(nextChannel, 3)
+        break
+      case 'NET_FOUR':
+        nextChannel = parseChannel(nextChannel, 4)
+        break
+      case 'NET_FIVE':
+        nextChannel = parseChannel(nextChannel, 5)
+        break
+      case 'NET_SIX':
+        nextChannel = parseChannel(nextChannel, 6)
+        break
+      case 'NET_SEVEN':
+        nextChannel = parseChannel(nextChannel, 7)
+        break
+      case 'NET_EIGHT':
+        nextChannel = parseChannel(nextChannel, 8)
+        break
+      case 'NET_NINE':
+        nextChannel = parseChannel(nextChannel, 9)
+        break
+      case 'NET_ZERO':
+        nextChannel = parseChannel(nextChannel, 0)
+        break
+
       // USEFUL BUTTONS
       case 'NET_CHANNELUP':
+        nextChannel++ // missing logic of the last channel
+        break
       case 'NET_CHANNELDOWN':
+        nextChannel--
+        isFinished = true // todo: delay? Change the channel everytime
+        break
       case 'NET_BACK':
+        if (!isMenu) {
+          channel = lastChannel
+          isFinished = true
+        }
+        break
       case 'NET_UP':
+        if (!isMenu) {
+          nextChannel++
+        }
+        break
       case 'NET_DOWN':
-      case 'NET_LEFT':
-      case 'NET_RIGHT':
       case 'SAM_POWER':
         break
       // EXTRA MENU NAVIGATION
+      case 'NET_LEFT':
+      case 'NET_RIGHT':
       case 'NET_PORTAL':
       case 'NET_MOSAIC':
       case 'NET_NETTV':
@@ -71,6 +113,23 @@ function interpretate (data) {
       case 'NET_REVERSE':
         break
     }
+    console.log('Switch end. Last Channel: ' + lastChannel + ' Channel: ' + channel + ' NextChannel: ' + nextChannel)
+    if (data[i + 1] && (data[i + 1].date.getTime() - data[i].date.getTime()) > 3000) {
+      lastChannel = channel
+      channel = nextChannel
+      nextChannel = 0
+      console.log('Modificando. Last Channel: ' + lastChannel + ' Channel: ' + channel + ' NextChannel: ' + nextChannel)
+    }
   }
 }
 exports.interpretate = interpretate
+
+// if channel has 3 digits and receive another digit, it means a wrong channel and the next channel will be the new digit
+// if channel has less than 3 digits, the new digit will be added to the next channel
+function parseChannel (nextChannel, number) {
+  if (nextChannel > 100) {
+    return number
+  } else {
+    return (nextChannel * 10) + number
+  }
+}
