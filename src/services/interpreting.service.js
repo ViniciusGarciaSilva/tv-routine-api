15dd93e8844ee54fffb60460f62e68da45b2f1c6
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2019-07-02 13:59:07 -0300

    Classification

--- src/services/interpreting.service.js
+++ src/services/interpreting.service.js
@@ -1,27 +1,7 @@
 function interpretate (data) {
   for (let i = 0; i < data.length; i++) {
     switch (data) {
-      case 'NET_PORTAL':
-      case 'NET_NET':
-      case 'NET_MOSAIC':
-      case 'NET_INFO':
-      case 'NET_VOLUMEUP':
-      case 'NET_VOLUMEDOWN':
-      case 'NET_MUTE':
-      case 'NET_BACK':
-      case 'NET_CHANNELUP':
-      case 'NET_CHANNELDOWN':
-      case 'NET_EXIT':
-      case 'NET_NETTV':
-      case 'NET_UP':
-      case 'NET_DOWN':
-      case 'NET_LEFT':
-      case 'NET_RIGHT':
-      case 'NET_ITV':
-      case 'NET_AUDIO':
-      case 'NET_RIGHTNOW':
-      case 'NET_SUBTITLE':
-      case 'NET_MESSAGES':
+      // CHANNEL
       case 'NET_ONE':
       case 'NET_TWO':
       case 'NET_THREE':
@@ -32,20 +12,20 @@ function interpretate (data) {
       case 'NET_EIGHT':
       case 'NET_NINE':
       case 'NET_ZERO':
-      case 'NET_FAVORITE':
+        break
+      // EXTRA MENU NAVIGATION
+      case 'NET_PORTAL':
+      case 'NET_MOSAIC':
+      case 'NET_NETTV':
+      case 'NET_ITV':
+      case 'NET_MESSAGES':
+      case 'NET_RIGHTNOW':
       case 'NET_MENU':
-      case 'NET_REVERSE':
-      case 'NET_PLAY':
-      case 'NET_FORWARD':
-      case 'NET_REPLAY':
-      case 'NET_STOP':
-      case 'NET_REC':
+      case 'NET_EXIT':
+      case 'NET_FAVORITE':
       case 'NET_PAYPERVIEW':
       case 'NET_NOW':
       case 'NET_MUSIC':
-      case 'SAM_POWER':
-      case 'SAM_NUMBERS':
-      case 'SAM_DOTS':
       case 'SAM_UP':
       case 'SAM_DOWN':
       case 'SAM_LEFT':
@@ -53,13 +33,43 @@ function interpretate (data) {
       case 'SAM_CENTER':
       case 'SAM_RETURN':
       case 'SAM_HOME':
+        break
+      // USEFUL BUTTONS
+      case 'NET_CHANNELUP':
+      case 'NET_CHANNELDOWN':
+      case 'NET_BACK':
+      case 'NET_UP':
+      case 'NET_DOWN':
+      case 'NET_LEFT':
+      case 'NET_RIGHT':
+      case 'SAM_POWER':
+        break
+      // USELESS BUTTONS
+      case 'NET_VOLUMEUP':
+      case 'NET_VOLUMEDOWN':
+      case 'NET_NET':
+      case 'SAM_VOLUME':
+      case 'SAM_CHANNEL':
+      case 'NET_INFO':
+      case 'NET_MUTE':
+      case 'NET_AUDIO':
+      case 'NET_SUBTITLE':
+      case 'SAM_NUMBERS':
+      case 'SAM_DOTS':
       case 'SAM_PLAYPAUSE':
       case 'SAM_VOLUMEUP':
       case 'SAM_VOLUMEDOWN':
       case 'SAM_CHANNELUP':
       case 'SAM_CHANNELDOWN':
-      case 'SAM_VOLUME':
-      case 'SAM_CHANNEL':
+        break
+      // RECORD
+      case 'NET_PLAY':
+      case 'NET_FORWARD':
+      case 'NET_REPLAY':
+      case 'NET_STOP':
+      case 'NET_REC':
+      case 'NET_REVERSE':
+        break
     }
   }
 }
