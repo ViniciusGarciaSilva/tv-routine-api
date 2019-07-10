8f34f3a3ebc9255139caa7440d15853122c5ee15
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2019-07-10 14:16:11 -0300

    Change server port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
-// Load HTTP module
-const hostname = 'localhost'
-const port = 8000
-
 const app = require('./app')
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+// Load HTTP module
+const port = normalizaPort(process.env.PORT || '5000')
+
+function normalizaPort (val) {
+  const port = parseInt(val, 10)
+  if (isNaN(port)) {
+    return val
+  }
+  if (port >= 0) {
+    return port
+  }
+  return false
+}
+
+app.listen(port, function () { // passa a porta a ser escutada e da um console log
+  console.log(`app listening on port ${port}`)
 })
